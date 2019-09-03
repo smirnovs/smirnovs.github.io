1cec8f2ba97b8af25667f42a03696f346ebbc315
Alexandr Smirnov <uzukwert@gmail.com>
2019-09-03 16:38:11 +0300

    Name Validation

---
+++ script.js
@@ -0,0 +1,9 @@
+// const form = document.forms.register;
+
+// const namePattern = form.elements.name.pattern;
+
+// const emaleForm = document.forms.name;
+// const phoneForm = document.forms.name;
+// const siteForm = document.forms.name;
+
+// console.log(namePattern);
